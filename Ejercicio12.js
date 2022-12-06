6d1572c0ff7697737311bfb6d3b933b2773dd730
uo283706 <113984425+uo283706@users.noreply.github.com>
2022-12-07 00:16:48 +0100

    Add files via upload

---
+++ Ejercicio12.js
@@ -0,0 +1,95 @@
+class lectorArchivos {
+    constructor(){
+        this.nFiles = 0;
+        this.fileArray;
+        this.totalBytes = 0;
+
+    }
+
+    checkearApiFile(){
+
+            // Version 1.1 23/10/2021 
+        if (!(window.File && window.FileReader && window.FileList && window.Blob)) 
+        {  
+            alert("Este navegador NO soporta la subida de archivos");
+            return;
+        
+        }
+
+    }
+    
+    infoArchivos() {
+            this.totalBytes = 0;
+            this.fileArray  = document.querySelector("input").files;
+            this.nFiles = this.fileArray.length;
+
+            var tit2 = "<h2>Info total de los archivos</h2>"
+        for (var i = 0; i <  this.nFiles; i++) {
+            this.totalBytes += this.fileArray[i].size;
+        }
+        var infoBasica="";
+        for (var i = 0; i <  this.nFiles; i++) {
+            infoBasica += "<p>Archivo[" + i +"] = "+ this.fileArray[i].name  + " Tamaño: " + this.fileArray[i].size +" bytes " + " Tipo: " + this.fileArray[i].type+"</p>" ;
+        }
+        
+
+        $('input').before("" + tit2 +" <p>Total de bytes: " + this.totalBytes + "<p>" + infoBasica);
+       
+        this.mostrarContenidoArchivos();
+    }
+
+    mostrarContenidoArchivos(){
+        var file;
+     
+        $('input').before("<h2>Contenido de los archivos</h2>");
+        for (var i = 0; i <  this.nFiles; i++) {
+            
+            
+            file = this.fileArray[i];
+            
+            this.leerArchivo(file);
+
+
+        }
+
+
+    }
+
+
+    leerArchivo(file){
+
+       var string = "<h3>"+file.name + "</h3>"
+       var lector ;
+       var regexJson = "application/json";
+       var regexTxt = "text/plain";
+       var regexxml = "text/xml";
+      
+            $('input').before("<section>"+string);
+
+
+           
+            if(file.type ===  regexJson || file.type === regexTxt || file.type === regexxml){
+                $("h3:last").after("<p name=\"" +  file.name + "\"></p></section>");
+
+                lector = new FileReader();
+
+                lector.onload = function(evento){
+
+                   
+                    document.querySelector("p[name=\"" + file.name + "\"").innerText = lector.result;
+                    
+                }
+
+                lector.readAsText(file);
+
+            }else{
+                string = "<p>Imposible leer archivo, formato no permitido.</p></section>";
+                $('h3:last').before(string);
+            }
+
+
+    }
+
+
+}
+var lector = new lectorArchivos();
